5132564e7983bd4d4a671991832655781acf24c9
tito82 <roberto.herna@gmail.com>
2020-07-07 15:04:03 -0600

    primero

---
+++ aap1/src/aap1/Aap1.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aap1;
+import java.util.Scanner;
+/**
+ *
+ * @author rober
+ */
+public class Aap1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+           
+        for (int i = 10; i > 0; i--) {
+            for (int x = 0; x < i; x++) {
+                    System.out.print("* ");
+            }
+            System.out.println("");
+        }
+       
+    
+        
+       /* 
+        
+        ****
+        ***
+        **
+        *
+        
+        
+        */
+      
+        
+        
+        
+        
+        
+    }
+    
+}
