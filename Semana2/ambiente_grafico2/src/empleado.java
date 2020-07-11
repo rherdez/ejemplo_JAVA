c1a10a3290d086c5a86eb044bff55ffe4605a419
tito82 <roberto.herna@gmail.com>
2020-07-10 19:39:29 -0600

    $m

---
+++ Semana2/ambiente_grafico2/src/empleado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rober
+ */
+public class empleado {
+    int id;
+    String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
