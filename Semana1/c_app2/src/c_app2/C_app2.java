1ab94215adb7f46e35cd20cca562013203146758
tito82 <roberto.herna@gmail.com>
2020-07-22 11:24:48 -0600

    $m

--- Semana1/c_app2/src/c_app2/C_app2.java
+++ Semana1/c_app2/src/c_app2/C_app2.java
@@ -16,38 +16,26 @@ public class C_app2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc=new Scanner(System.in);
-        int n;
-        
-        System.out.println("Ingrese un numero 1-7");
-        n=sc.nextInt();
-        
-        switch(n){
-            case 1:
-                    System.out.println("Domingo");
-                    break;
-            case 2: 
-                    System.out.println("Lunes");
-                    break;
-            case 3:
-                    System.out.println("Martes");
-                    break;
-            case 4:
-                    System.out.println("Miercoles");
-                    break;
-            case 5:
-                    System.out.println("Jueves");
-                    break;
-            case 6:
-                    System.out.println("Viernes");
-                    break;
-            case 7:
-                   System.out.println("Sabado");     
+       
+            Scanner ct= new Scanner (System.in);
+            char letra;
+
+            System.out.println("Ingrese una letra");
+            letra=ct.next().charAt(0);
+
+            switch(letra)
+            {
+                case 'a':                            
+                case 'e':                
+                case 'i':               
+                case 'o':               
+                case 'u':
+                   System.out.println("Es vocal");
                    break;
-            default:
-                System.out.println("opcion no valida");
-                  
-        }   
+                default: 
+                   System.out.println("Es una consonante");
+            }   
+                    
     }
     
 }
