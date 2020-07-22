0080c291cfed25a730da4bf0ee12720deabacdfb
tito82 <roberto.herna@gmail.com>
2020-07-22 10:19:27 -0600

    $m

---
+++ Semana1/c_app1/src/c_app1/C_app1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c_app1;
+import java.util.Scanner;
+/**
+ *
+ * @author rober
+ */
+public class C_app1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.print("Ingrese un numero");
+        n=sc.nextInt();
+        System.out.println("El numero es: "+ n);
+    }
+    
+}
