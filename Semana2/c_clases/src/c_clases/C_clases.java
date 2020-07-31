f7810ec11bdfd921436ba4f1800c83103fbe3651
tito82 <roberto.herna@gmail.com>
2020-07-31 11:32:31 -0600

    $m

--- Semana2/c_clases/src/c_clases/C_clases.java
+++ Semana2/c_clases/src/c_clases/C_clases.java
@@ -14,12 +14,38 @@ public class C_clases {
     /**
      * @param args the command line arguments
      */
+    static mouse []klip=new mouse[4];
+    static void ordenar(){
+        mouse temp=new mouse();
+        
+        for(int i=0;i<4;i++){
+            for(int x=1;x<4;x++){
+                if(klip[x-1].N_botones>klip[x].N_botones){
+                   
+                    
+                    temp.N_botones=klip[x].N_botones;
+                    temp.color=klip[x].color;
+                    
+                    klip[x].N_botones=klip[x-1].N_botones;
+                    klip[x].color=klip[x-1].color;
+                    
+                    klip[x-1].N_botones=temp.N_botones;
+                    klip[x-1].color=temp.color;
+                                                            
+                }
+                
+            }
+        }
+        
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc=new Scanner(System.in);
-        mouse []klip=new mouse[2];
+        
      
-        for(int i=0;i<2;i++){
+        for(int i=0;i<4;i++){
             klip[i]=new mouse();
             System.out.println("Ingrese Color Mouse");
             klip[i].set_color(sc.next());
@@ -28,10 +54,10 @@ public class C_clases {
             klip[i].set_Nbotones(sc.nextInt());
         }
         ////
-      
+      ordenar();
       ///prsentar
         
-      for(int i=0;i<2;i++){
+      for(int i=0;i<4;i++){
         System.out.println("El color es: "+ klip[i].get_color());
         System.out.println("El numero de botones es: "+klip[i].get_Nbotones());
       }
