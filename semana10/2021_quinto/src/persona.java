a8326dea7c9b6a152567c740435ddee5bb7d3486
herdezclase <herdez.clase@gmail.com>
2021-02-13 16:35:25 -0600

    $m

---
+++ semana10/2021_quinto/src/persona.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rober
+ */
+public class persona {
+    String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    int edad;
+    
+}
